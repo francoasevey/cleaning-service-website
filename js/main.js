4feb6c691e03f22a3094d2c848ee0fff0f031961
francoromeo <francoromeo5012@gmail.com>
2024-01-15 12:20:59 -0300

    medias

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ const headerBg = () =>{
     });
 }
 headerBg();
+
+const navigation = () =>{
+    const navToggler = document.querySelector(".js-nav-toggler");
+    const nav = document.querySelector(".js-nav");
+    const navItems = document.querySelectorAll("li");
+
+    const navToggle = () =>{
+        nav.classList.toggle("open");
+        navToggler.classList.toggle("active");
+    }
+    navToggler.addEventListener("click", navToggle);
+
+    navItems.forEach((li) => {
+        li.querySelector("a").addEventListener("click", () => {
+            if(window.innerWidth <= 767){
+                navToggle();
+            }
+        });
+    });
+}
+navigation();
